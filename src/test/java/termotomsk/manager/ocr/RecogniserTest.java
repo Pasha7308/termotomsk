1796e75137e6b0eeea0fd7af6836add08e49b1d1
Pasha7308 <pasha7308@mail.ru>
2023-04-18 21:15:47 +0700

    add test files

--- src/test/java/termotomsk/manager/ocr/RecogniserTest.java
+++ src/test/java/termotomsk/manager/ocr/RecogniserTest.java
@@ -5,9 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
 
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class RecogniserTest {
@@ -16,6 +17,12 @@ class RecogniserTest {
 
     @Test
     void recognize() {
+        File folder = new File(System.getProperty("user.dir") + "/src/test/resources/testImages");
+        File[] files = folder.listFiles();
+
+        assertNotNull(files);
+        assertEquals(files.length, 26);
+
         var image = new BufferedImage(100, 10, TYPE_INT_RGB);
 
         var result = recogniser.recognize(image);
