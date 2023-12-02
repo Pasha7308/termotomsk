aa059d1805fac1193c760e8c4f30c6dd671fcd9c
Pasha7308 <pasha7308@mail.ru>
2023-12-02 19:56:19 +0700

    add some logs

--- src/main/java/termotomsk/manager/Scheduler.java
+++ src/main/java/termotomsk/manager/Scheduler.java
@@ -1,6 +1,7 @@
 package termotomsk.manager;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import termotomsk.manager.downloader.DownloadTermo;
@@ -10,15 +11,17 @@ import java.time.OffsetDateTime;
 
 import static java.time.temporal.ChronoUnit.MILLIS;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class Scheduler {
-    final static private int period = 60 * 1000;
+    final static private int PERIOD = 60 * 1000;
     private final WeatherContainer weatherContainer;
 
     @SuppressWarnings("BusyWait")
-    @Scheduled(initialDelay = 10 * 1000, fixedRate = period)
+    @Scheduled(initialDelay = 10 * 1000, fixedRate = PERIOD)
     public void reportCurrentTime() {
+        log.info("Termo start");
         var weather = weatherContainer.getWeather();
 
         weather.setUpdated(OffsetDateTime.now());
@@ -36,9 +39,10 @@ public class Scheduler {
             weatherToSave.assign(weather);
             weatherContainer.addToQueue(weatherToSave);
         }
+        log.info("Termo finish. Temp: {}", weather.getServerTermo().getTemp());
     }
 
     private boolean isTooLong(OffsetDateTime started) {
-        return started.plus(period, MILLIS).isBefore(OffsetDateTime.now());
+        return started.plus(PERIOD, MILLIS).isBefore(OffsetDateTime.now());
     }
 }
