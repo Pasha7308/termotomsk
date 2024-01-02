b407b4060ff66f3bb487fab300b896b0862256c6
Pasha7308 <pasha7308@mail.ru>
2024-01-02 18:23:23 +0700

    up all dependencies

--- src/main/java/termotomsk/manager/SchedulerYanadex.java
+++ src/main/java/termotomsk/manager/SchedulerYanadex.java
@@ -4,10 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import termotomsk.api.yandex.dto.YandexWeather;
+import termotomsk.api.yandex.dto.YandexWeatherFact;
+import termotomsk.api.yandex.dto.YandexWeatherInfo;
 import termotomsk.manager.downloader.DownloaderYandex;
 import termotomsk.model.Weather;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 @Component
@@ -24,6 +29,7 @@ public class SchedulerYanadex {
 
         weather.setUpdated(OffsetDateTime.now());
 
+//        var yandexWeather = getFakeYandex();
         var yandexWeather = downloaderYandex.getYandexWeather();
         weather.getServerYandex().setTemp(Double.valueOf(yandexWeather.getFact().getTemp() * 10.0).intValue());
         weather.getServerYandex().setUpdated(OffsetDateTime.now());
@@ -33,5 +39,18 @@ public class SchedulerYanadex {
             weatherContainer.addToQueue(weatherToSave);
         }
         log.info("Yandex finish. Temp: {}", weather.getServerYandex().getTemp());
+    }
+
+    private YandexWeather getFakeYandex() {
+        var yandexWeather = new YandexWeather();
+        yandexWeather.setNowDt(LocalDateTime.now());
+        yandexWeather.setInfo(new YandexWeatherInfo());
+        yandexWeather.setFact(new YandexWeatherFact());
+        yandexWeather.getFact().setTemp(nextDoubleBetween(-30.0, 30.0));
+        return yandexWeather;
+    }
+
+    public static double nextDoubleBetween(double min, double max) {
+        return (ThreadLocalRandom.current().nextDouble() * (max - min)) + min;
     }
 }
